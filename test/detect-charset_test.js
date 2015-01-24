9efa9cf4b6dd88b50e48c32daaaafa1c1cb52129
Trey Hunner <trey@treyhunner.com>
2015-01-24 13:59:53 -0800

    Add test for latin1 files

--- test/detect-charset_test.js
+++ test/detect-charset_test.js
@@ -2,11 +2,17 @@
 
 var detectCharset = require('../');
 var assert = require('should');
+var fs = require('fs');
+var path = require('path');
+
+function getFixture(fixture) {
+  return fs.readFileSync(path.join(__dirname, 'fixtures', fixture));
+}
 
 describe('detectCharset', function () {
 
-  it('should be awesome', function () {
-    detectCharset().should.equal('awesome');
+  it('should return latin1 for non-unicode files', function () {
+    assert.equal(detectCharset(getFixture('latin1.txt')), 'latin1');
   });
 
 });
